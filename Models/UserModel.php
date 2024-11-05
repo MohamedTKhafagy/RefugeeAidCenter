50d79f33f2727a5854f25c28b31f92ee6950d68a
Mohamed Khafagy <mohamed.tarek.khafagy@gmail.com>
2024-11-05 14:46:40 +0200

    Added code to the User class

---
+++ Models/UserModel.php
@@ -0,0 +1,19 @@
+<?php
+
+abstract class User{
+    private $Id;
+    private $Name;
+    private $Age;
+    private $Gender;
+    private $Address;
+    private $Phone;
+    private $Nationality;
+    private $Type;
+    private $Email;
+    private $Preference;
+
+
+
+    abstract public function RegisterEvent();
+
+}
