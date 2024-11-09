ef7fd64e31d2beda45f30a7f136686bf98ccb8cb
Mohamed Amin <mamin7522@gmail.com>
2024-11-09 20:32:57 +0200

    added shelters list view

---
+++ Views/ShelterListView.php
@@ -0,0 +1,37 @@
+<!-- Views/ShelterListView.php -->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Shelter List</title>
+</head>
+
+<body>
+    <h1>Shelters</h1>
+    <table border="1">
+        <tr>
+            <th>Shelter ID</th>
+            <th>Name</th>
+            <th>Address</th>
+            <th>Supervisor</th>
+            <th>Max Capacity</th>
+            <th>Current Capacity</th>
+            <th>Actions</th>
+        </tr>
+        <?php foreach ($shelters as $shelter) : ?>
+            <tr>
+                <td><?= $shelter['ShelterID'] ?></td>
+                <td><?= $shelter['Name'] ?></td>
+                <td><?= $shelter['Address'] ?></td>
+                <td><?= $shelter['Supervisor'] ?></td>
+                <td><?= $shelter['MaxCapacity'] ?></td>
+                <td><?= $shelter['CurrentCapacity'] ?></td>
+                <td>
+                    <a href="index.php?action=show&ShelterID=<?= $shelter['ShelterID'] ?>">View</a>
+                </td>
+            </tr>
+        <?php endforeach; ?>
+    </table>
+</body>
+
+</html>
