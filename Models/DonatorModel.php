9c163b33082fa71225e25de933cc6bb7bc0a1d23
Mohamed Khafagy <mohamed.tarek.khafagy@gmail.com>
2024-11-09 23:31:36 +0200

    Added Constructor to donator and updated user model to have protected variables

--- Models/DonatorModel.php
+++ Models/DonatorModel.php
@@ -3,11 +3,18 @@ require_once "UserModel.php";
 
 class Donator extends User{
     private $DonatorID;
+
+
+    public function __construct($Id, $Name, $Age, $Gender, $Address, $Phone, $Nationality, $Type, $Email, $Preference)
+    {
+        parent::__construct($Id, $Name, $Age, $Gender, $Address, $Phone, $Nationality, $Type, $Email, $Preference);
+    }
+
     //Type: 0 = Money, 1 = Clothes, 2 = Food
-    //DirectedTo: 0 = Hospital, 1 = School, 2 = Shelter
+    //DirectedTo: 0 = Hospital, 1 = School, 2 = Shelter (Default)
     //Collection: 0 = No Collection Fee, 1 = Add Collection Fee (Default)
     //Currency: 0 = EGP (Default), 1 = USD, 2 = GBP
-    public function MakeDonation($Type,$Amount,$DirectedTo,$Collection=1,$currency=0){
+    public function MakeDonation($Type,$Amount,$DirectedTo=2,$Collection=1,$currency=0){
        $donation =new Donation($Type,$Amount,$DirectedTo,$Collection,$currency);
        $status = $donation->Donate();
        if($status){
@@ -26,4 +33,9 @@ class Donator extends User{
     {
         //to be done when events are implemented    
     }
+    public function Update()
+    {
+        //to be implemented
+    }
 }
+
--- Models/UserModel.php
+++ Models/UserModel.php
@@ -1,19 +1,69 @@
 <?php
 
-abstract class User{
-    private $Id;
-    private $Name;
-    private $Age;
-    private $Gender;
-    private $Address;
-    private $Phone;
-    private $Nationality;
-    private $Type;
-    private $Email;
-    private $Preference;
+abstract class User
+{
 
+    // User properties
+    protected $Id;
+    protected $Name;
+    protected $Age;
+    protected $Gender;
+    protected $Address;
+    protected $Phone;
+    protected $Nationality;
+    protected $Type; // Could be 'refugee', 'volunteer', etc.
+    protected $Email;
+    protected $Preference; // Some preferences specific to the user
+
+    // Constructor to initialize user data
+    public function __construct($Id, $Name, $Age, $Gender, $Address, $Phone, $Nationality, $Type, $Email, $Preference)
+    {
+        $this->Id = $Id;
+        $this->Name = $Name;
+        $this->Age = $Age;
+        $this->Gender = $Gender;
+        $this->Address = $Address;
+        $this->Phone = $Phone;
+        $this->Nationality = $Nationality;
+        $this->Type = $Type;
+        $this->Email = $Email;
+        $this->Preference = $Preference;
+    }
 
 
     abstract public function RegisterEvent();
+    abstract public function Update();
 
+
+
+    // Method to display a brief user information
+    public function displayInfo()
+    {
+        return "Name: $this->Name, Age: $this->Age, Gender: $this->Gender, Nationality: $this->Nationality, Email: $this->Email";
+    }
+
+    public function getRefugeeName()
+    {
+        return $this->Name;
+    }
+
+    public function getRefugeeAge()
+    {
+        return $this->Age;
+    }
+
+    public function getRefugeeGender()
+    {
+        return $this->Gender;
+    }
+
+    public function getRefugeeNationality()
+    {
+        return $this->Nationality;
+    }
+
+    public function getRefugeeID()
+    {
+        return $this->Id;
+    }
 }
