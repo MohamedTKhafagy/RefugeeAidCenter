b0446fb05893d233e608fd38fe9f677124f112e6
Mohamed Khafagy <mohamed.tarek.khafagy@gmail.com>
2024-11-12 09:26:32 +0200

    Added setters and getters.

--- Models/UserModel.php
+++ Models/UserModel.php
@@ -42,28 +42,80 @@ abstract class User
         return "Name: $this->Name, Age: $this->Age, Gender: $this->Gender, Nationality: $this->Nationality, Email: $this->Email";
     }
 
-    public function getRefugeeName()
+    public function getName()
     {
         return $this->Name;
     }
 
-    public function getRefugeeAge()
+    public function getAge()
     {
         return $this->Age;
     }
 
-    public function getRefugeeGender()
+    public function getGender()
     {
         return $this->Gender;
     }
 
-    public function getRefugeeNationality()
+    public function getNationality()
     {
         return $this->Nationality;
     }
 
-    public function getRefugeeID()
+    public function getID()
     {
         return $this->Id;
+    }
+    public function getAddress(){
+        return $this->Address;
+    }
+    public function getPhone(){
+        return $this->Phone;
+    }
+    public function getType(){
+        return $this->Type;
+    }
+    public function getPreference(){
+        return $this->Preference;
+    }
+    public function getEmail(){
+        return $this->Email;
+    }
+    
+    public function setName($Name){
+        $this->Name = $Name;
+        return true;
+    }
+    public function setAge($Age){
+        $this->Age = $Age;
+        return true;
+    }
+    public function setGender($Gender){
+        $this->Gender = $Gender;
+        return true;
+    }
+    public function setAddress($Address){
+        $this->Address = $Address;
+        return true;
+    }
+    public function setPhone($Phone){
+        $this->Phone = $Phone;
+        return true;
+    }
+    public function setNationality ($Nationality){
+        $this->Nationality = $Nationality;
+        return true;
+    }
+    public function setType($Type){
+        $this->Type = $Type;
+        return true;
+    }
+    public function setEmail($Email){
+        $this->Email = $Email;
+        return true;
+    }
+    public function setPreference($Preference){
+        $this->Preference = $Preference;
+        return true;
     }
 }
