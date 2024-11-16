e6d11798b5214fe1cb321b599e94b5ed0d37974e
Mohamed Khafagy <mohamed.tarek.khafagy@gmail.com>
2024-11-16 23:56:02 +0200

    Refactored code related to showing Inventory details

--- Controllers/InventoryController.php
+++ Controllers/InventoryController.php
@@ -4,19 +4,9 @@ require_once __DIR__ . '/../Models/InventoryModel.php';
 
 class InventoryController
 {
-    public function showAllInventory()
-    {
-        $inventoryItems = Inventory::getAll();
-        include __DIR__ . '/../views/InventoryListView.php';
-    }
-
     public function showInventory()
     {
         $inventory = new Inventory();
-        if ($inventory) {
-            include __DIR__ . '/../views/InventoryDetailView.php';
-        } else {
-            echo "Inventory item not found.";
-        }
+        include __DIR__ . '/../views/InventoryDetailView.php';
     }
 }
