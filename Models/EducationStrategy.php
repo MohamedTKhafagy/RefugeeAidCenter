0dcb64f046d34c2970fd4afc62d2a05e0053d0d5
AhmedWael02 <ahmedwael2062@gmail.com>
2024-11-16 22:26:45 +0200

    Education Strategy Interface

--- Models/EducationStrategy.php
+++ Models/EducationStrategy.php
@@ -1,5 +1,9 @@
 <?php
-
 //EducationStrategy.php
 
+interface EducationStrategy {
+    public function AssignSchool($school);
+    public function AssignClass($class);
+    public function AssignTeacher($teacher);
+}
 
