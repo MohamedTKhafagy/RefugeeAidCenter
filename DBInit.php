52c013c1e2275fbbae5a0ca52328830f2659a0db
OsamaKhaled9 <osamakhaled236@gmail.com>
2024-11-17 14:06:57 +0200

    Added DB Initialization

---
+++ DBInit.php
@@ -0,0 +1,56 @@
+<?php
+require_once "SingletonDB.php";
+//This file is to create the tables if running for the first time 
+$db = DbConnection::getInstance();
+$db->query("
+   CREATE TABLE IF NOT EXISTS Address (
+    Id INT AUTO_INCREMENT PRIMARY KEY,
+    Name VARCHAR(30),
+    ParentId INT,
+    FOREIGN KEY (ParentId) REFERENCES Address(Id)
+); 
+");
+$db->query("
+   CREATE TABLE IF NOT EXISTS User (
+    Id INT AUTO_INCREMENT PRIMARY KEY,
+    Name VARCHAR(255) NOT NULL,
+    Age INT,
+    Gender INT,
+    Address INT,
+    Phone VARCHAR(20),
+    Nationality VARCHAR(30),
+    Type INT, 
+    Email VARCHAR(255) UNIQUE,
+    Preference INT,
+    IsDeleted INT DEFAULT 0,
+    FOREIGN KEY (Address) REFERENCES Address(Id)
+); 
+");
+$db->query("
+   CREATE TABLE IF NOT EXISTS Donation (
+    Id INT AUTO_INCREMENT PRIMARY KEY,
+    Amount INT,
+    Type INT,
+    DirectedTo INT,
+    Collection INT,
+    Currency INT
+);
+");
+
+$db->query("
+    CREATE TABLE IF NOT EXISTS DonatorDonation (
+     DonatorId INT NOT NULL,
+     DonationId INT NOT NULL,
+     PRIMARY KEY (DonatorId, DonationId),
+     FOREIGN KEY (DonatorId) REFERENCES User(Id),
+     FOREIGN KEY (DonationId) REFERENCES Donation(Id)
+);
+");
+$db->query("
+    CREATE TABLE IF NOT EXISTS INVENTORY (
+     Money INT NOT NULL,
+     ClothesQuantity INT NOT NULL,
+     FoodResourcesQuantity INT NOT NULL
+);
+");  
+?>
